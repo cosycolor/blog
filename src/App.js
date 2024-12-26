3699d41a3bc7f9cd00f5ba2f41a72f9729c2f09c
dyhan <handyoon87@gmail.com>
2024-12-26 13:48:54 +0900

    app.js only

---
+++ src/App.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  let [title, titleChange] = useState(['남자코트 추천','강남 우동 맛집','파이썬독학']);
+  let [like, plusLike] = useState(0);
+
+  function addLike(){
+    plusLike(like + 1);
+  }
+
+  function changeTitle(){
+    let copy = [...title];
+    copy[0] = '여자';
+    titleChange(copy);
+  }
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>blog</h4>
+      </div>
+      <button onClick={ changeTitle }>edit </button>
+      <div className = "list">
+        <h4>{title[0]} <span onClick={ addLike }>👍</span> {like} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className = "list">
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className = "list">
+        <h4>{title[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default App;
