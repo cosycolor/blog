71cc9823496cce222c3cb25b214654eed00b019a
cosycolor <handyoon87@gmail.com>
2024-12-27 00:02:45 +0900

    input delete array Using

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   let [like, plusLike] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
   let [number, setNumber] = useState(0);
+  let [input, setInput] = useState('e');
   
 
   function addLike(e){
@@ -21,6 +22,17 @@ function App() {
     let copy = [...title];
     copy[0] = '여자코트 추천';
     titleChange(copy);
+  }
+
+  function saveInput(e){
+    setInput(e.target.value);
+  }
+
+  function saveTitle(){
+    changeTitle(title.push(input));
+    let copy = [...like];
+    copy.push(0);
+    plusLike(copy);
   }
 
 
@@ -38,11 +50,20 @@ function App() {
                 {title[i]} <span onClick={ (e) => {e.stopPropagation(), addLike(i)} }>👍</span> {like[i]}
               </h4>
               <p>2월 17일 발행</p>
+              <button onClick = { () => {
+                let copy = [...title];
+                copy.splice(i,1);
+                titleChange(copy);
+                let copyLike = [...like];
+                copyLike.splice(i,1);
+                plusLike(copyLike);
+              }}>삭제</button>
             </div>)
         })
       /* onClick 이벤트 파라미터 전달 시 () => 함수이름, 파라미터 없을 시 그냥 함수이름 이렇게 해야하는건지? 
       ()=>{} 이렇게 호출하는거와 단순 function 이름으로 호출하는 경우의 차이는 뭔지*/}
       <hr></hr>
+      <input onChange = { (e) => {setInput(e.target.value)}}></input><button onClick = { () => {saveTitle()}}>글 발행</button>
       {
                   //props으로 부모 -> 자식 파라미터 전달, 함수, state, 변수 다양하게 전달 가능
                   modal == true ? <Modal titleChange = {titleChange} title = {title} number = {number}/> : null
